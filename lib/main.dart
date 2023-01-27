834eb92872415b3c70df4d4e24e0f9b516ac9415
triagung128 <triagung128@gmail.com>
2023-01-27 14:50:24 +0700

    set false showDebugBanner

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const CupertinoApp(
       title: 'Notepad App',
+      debugShowCheckedModeBanner: false,
       theme: CupertinoThemeData(
         primaryColor: CupertinoColors.systemBlue,
       ),
